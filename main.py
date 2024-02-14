0ed3fb942babde6699f9598f27d28d56b2b19a85
k2angel <90847045+k2angel@users.noreply.github.com>
2024-02-15 04:25:56 +0900

    アニメーションgifに対応

--- main.py
+++ main.py
@@ -10,22 +10,35 @@ def main(file):
     except UnidentifiedImageError:
         print("ERROR!!")
         return
-    print(f"FileName: , {os.path.basename(file)}\nImageSize: {img.size}", sep="")
+    print(f"FileName: {os.path.basename(file)}\nImageSize: {img.size}")
     width, height = img.size
     if width > height:  # Wide
         space = int((width - height) / 2)
         hs = height + space
         print(height, space, hs, sep=", ")
-        artwork = img.crop((space, 0, hs, height))
         print(space, 0, hs, height, sep=", ")
+        crop_box = (space, 0, hs, height)
     else:  # Tall
         space = int((height - width) / 2)
         ws = width + space
         print(width, space, ws, sep=", ")
-        artwork = img.crop((0, space, width, ws))
         print(0, space, width, ws, sep=", ")
-    artwork.save(os.path.join("./img", os.path.basename(file)))
+        crop_box = (0, space, width, ws)
+    path = os.path.join("./img", os.path.basename(file))
+    if os.path.splitext(file)[1] == ".gif":
+        artwork = gif(img, crop_box)
+        artwork[0].save(path, save_all=True, append_images=artwork[1:], duration=1000 / img.info["duration"], loop=0)
+    else:
+        artwork = img.crop(crop_box)
+        artwork.save(path)
 
+def gif(img, crop_box):
+    cropped_imgs = []
+    for i in range(img.n_frames):
+        img.seek(i)  # ここで image が参照しているフレームが変わっている
+        cropped = img.crop(crop_box)
+        cropped_imgs.append(cropped)
+    return cropped_imgs
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
